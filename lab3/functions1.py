007c87ff7dd783195c0d8cf1ec9b707f3e398f8f
ansar <ansstsvv@gmail.com>
2025-02-02 15:39:57 +0500

    functions 1 completed

--- lab3/functions1.py
+++ lab3/functions1.py
@@ -30,15 +30,79 @@ primes = prime(numbers)
 print(primes)
 
 #5
+def permutations(string):
+    def permute(s, l, r):
+        if l == r:
+            perms.append(''.join(s))
+        else:
+            for i in range(l, r + 1):
+                s[l], s[i] = s[i], s[l]  # swap
+                permute(s, l + 1, r)
+                s[l], s[i] = s[i], s[l]  # backtrack
+
+    perms = []
+    permute(list(string), 0, len(string) - 1)
+    return perms
+
+#6
+def string_rev(string):
+    reversed_words = ' '.join(string.split()[::-1])
+    return reversed_words
+
+#7
+def has_33(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] == 3 and nums[i + 1] == 3:
+            return True
+    return False
+
+#8
+def spy_game(nums):
+    if nums.count(0) == 2 and nums.count(7) == 1:
+        return True
+    return False
+
+#9
+def volume(radius):
+    v = r * r * r * 3.14159265358979323846 * 4 / 3
+    return v
+
+#10
+def unique_list(lst):
+    new_lst = []
+    for i in lst:
+        if lst.count(i) == 1:
+            new_lst.append(i)
+    return new_lst
+
+#11
+def ispalindrome(string):
+    return string == string[::-1]
+
+#12
+def histogram(lst):
+    for i in lst:
+        print(i * '*')
+
+#13
 import random
-string = input("Enter a string ")
-n = 0
-ans = []
-fact = 1
-l = len(string)
-for x in range(1, l+1):
-    fact *= x
-print('Number of permutations: ', fact)
+def guess():
+    name = input("Hello! What is your name?")
+    print("Well, KBTU, I am thinking of a number between 1 and 20.")
+    num = random.randint(1, 20)
+    g = -1
+    while g != num:
+        g = int(input("Take a guess."))
+        if g < num:
+            print("Your guess is too low.")
+        elif g > num:
+            print("Your guess is too high.")
+        else:
+            break
+    print("Good job, " + name + "! You guessed my number in " + str(g) + " guesses!")
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-import randomcat
-string = input("Enter a string ")
-n = 0
-ans = []
-fact = 1
-l = len(string)
-for x in range(1, l+1):
-    fact *= x
-print('Number of permutations: ', fact)
-
-while n < fact:
-    ns = ''.join(random.sample(string,len(string)))
-    if ns not in ans:
-        ans.append(ns)
-        n += 1
-print(ans)
+s = "asdasd  asda qwe"
+reversed_words = ' '.join(s.split()[::-1])
+print(reversed_words)
