bd181d3011e5ccc26495cc88f9bf2f21bd3eb0c2
ansar <ansstsvv@gmail.com>
2025-02-05 17:20:56 +0500

    нашаманиол

--- lab5/rege.py
+++ lab5/rege.py
@@ -1,1 +1,9 @@
 import re
+file_path = r"C:\Users\Ansar\pepepeton\pp2-labs\lab5\sample3.txt"
+with open(file_path, 'r', encoding="utf8") as file:
+    data = file.read()
+
+
+#1
+first = re.findall("a.*b", data)
+print(first)
