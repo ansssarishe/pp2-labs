57839dbbe0296e4775b3c9b6f9b2670caf1f2cce
ansar <ansstsvv@gmail.com>
2025-02-05 16:25:05 +0500

    Lab4 completed

--- lab4/jsonnnn.py
+++ lab4/jsonnnn.py
@@ -1,1 +1,18 @@
-hz, potom reshu
+import json
+
+
+with open('/Users/ansstsvv/pp2/lab4/sample-data.json', 'r') as file:
+    data = json.load(file)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':<50} {'Description':<20} {'Speed':<10} {'MTU':<10}")
+print("-" * 80)
+
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
+    dn = attributes["dn"]
+    description = attributes["descr"]
+    speed = attributes["speed"]
+    mtu = attributes["mtu"]
+    print(f"{dn:<50} {description:<20} {speed:<10} {mtu:<10}")
