4baaa95f4d53b6eb9dd857dc2446536dd50b0e0e
ansar <ansstsvv@gmail.com>
2025-04-21 16:33:48 +0500

    asdasd

--- raycasting/map.py
+++ raycasting/map.py
@@ -1,3 +1,14 @@
 import pygame as pg 
 from settings import *
+from main import *
 
+
+
+
+
+class Map:
+    def __init__(self, game):
+        self.game = game
+        self.mini_map = mini_map
+        self.world_map = {}
+        self.get_map()
